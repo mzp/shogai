6a60c6a3110e6f2befaa39fb1378bf5ccfd1d79c
mzp <mzpppp@gmail.com>
2010-10-13 22:59:25 +0900

    rename

---
+++ app.rb
@@ -0,0 +1,21 @@
+# -*- mode:ruby; coding:utf-8 -*-
+
+require 'rubygems'
+require 'sinatra'
+require 'haml'
+
+require_relative 'model'
+require_relative 'crowler'
+
+set :haml, :format => :html5
+set :views, File.dirname(__FILE__) + '/views'
+set :public, File.dirname(__FILE__) + '/public'
+get '/' do
+   haml :index
+end
+
+get '/search' do
+  @date = Date.new params[:year].to_i,params[:month].to_i, params[:day].to_i
+  @institutions = Crowler.new.find_by_date @date
+  haml :search
+end
